61cc70d107cbf1311994590bcd5bc8de86034005
Iv17 <ivana.unitedforce@gmail.com>
2017-04-13 11:51:23 +0200

    grid_list

--- src/webapp/app/app.module.js
+++ src/webapp/app/app.module.js
@@ -4,15 +4,21 @@
     angular
         .module('nvtApp', [
             'ngResource',
+            'ngRoute',
             'ui.bootstrap',
             'ui.router'
         ])
-        .run(run);
-
-    <!-- vidi sta je ovo-->
-    run.$inject = ['stateHandler'];
-
-    function run(stateHandler) {
-        stateHandler.initialize();
-    }
+        .run(['Restangular', '$log', function(Restangular, $log) {
+    // postavimo base url za Restangular da ne bismo morali da ga
+    // navodimo svaki put kada se obracamo back endu
+    // poziv vrsimo na http://localhost:8080/api/
+    Restangular.setBaseUrl("api");
+    Restangular.setErrorInterceptor(function(response) {
+      if (response.status === 500) {
+        $log.info("internal server error");
+        return true; // greska je obradjena
+      }
+      return true; // greska nije obradjena
+    });
+  }]);
 })();
